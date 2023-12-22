4da35e4e4964a576b61175dc19df635350044df1
Jatin Yadav <jatin.yadav@cldcvr.com>
2023-12-22 15:54:53 +0530

    Bump gnupg version and pgp decryption changes

--- airbyte-integrations/connectors/source-file/setup.py
+++ airbyte-integrations/connectors/source-file/setup.py
@@ -22,7 +22,7 @@ MAIN_REQUIREMENTS = [
     "xlrd==2.0.1",
     "openpyxl==3.0.10",
     "pyxlsb==1.0.9",
-    "python-gnupg==0.5.1",
+    "python-gnupg==0.5.2",
 ]
 
 TEST_REQUIREMENTS = ["requests-mock~=1.9.3", "pytest~=6.2", "pytest-docker~=2.0.1", "pytest-mock~=3.6.1"]
--- airbyte-integrations/connectors/source-file/source_file/encryption_client/pgp.py
+++ airbyte-integrations/connectors/source-file/source_file/encryption_client/pgp.py
@@ -1,10 +1,12 @@
 #
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
-
-from io import BytesIO
+import logging
 
 import gnupg
+from airbyte_cdk.entrypoint import logger
+
+logging.getLogger("gnupg").setLevel(logging.DEBUG)
 
 
 class Pgp:
@@ -13,9 +15,19 @@ class Pgp:
         self.passphrase = passphrase
         self.encryption_method = encryption_method
 
-    def decrypt(self, fp):
-        gpg = gnupg.GPG("/usr/bin/gpg")
+    def decrypt(self, fp, output):
+        logger.info("start decrypt")
+        gpg = gnupg.GPG()
+
+        logger.info("GPG FOUND")
+        logger.info(f"gnupg Version: {gnupg.__version__}")
+        logger.info(f"version:{gpg.version}")
+
         import_result = gpg.import_keys(self.private_key)
+        logger.info("PRIVATE KEY IMPORTED")
+
         gpg.trust_keys(import_result.fingerprints, "TRUST_ULTIMATE")
-        decrypt = gpg.decrypt_file(fp, passphrase=self.passphrase)
-        return BytesIO(bytes(str(decrypt), "utf-8"))
+        logger.info("Trust key added")
+
+        with fp as encrypted_file:
+            _ = gpg.decrypt_file(encrypted_file, passphrase=self.passphrase, always_trust=True, output=output)
