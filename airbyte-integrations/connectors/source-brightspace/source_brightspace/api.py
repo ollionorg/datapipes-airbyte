4f0515ad9d2d788024c40ff8257a52b55164ee73
Jatin Yadav <107487891+jatinyadav-cc@users.noreply.github.com>
2024-03-28 20:02:20 +0530

    feat: source-brightspace refresh token key rotation (#25)
    * feat:source-brightspace refresh token key rotation
    
    * resolved commit
    
    * resolved commit
    
    * resolved commit
    
    * token generate process changed

--- airbyte-integrations/connectors/source-brightspace/source_brightspace/api.py
+++ airbyte-integrations/connectors/source-brightspace/source_brightspace/api.py
@@ -1,7 +1,6 @@
-import json
 import logging
 from enum import Enum
-from functools import lru_cache, wraps
+from functools import wraps
 from typing import Any, List
 
 import requests
@@ -46,13 +45,32 @@ def handle_http_errors(func):
             if error.response.status_code in [codes.TOO_MANY_REQUESTS]:
                 message = "API call-rate limit exceeded."
                 args[0].logger.error(message)
-            if error.response.status_code == codes.UNAUTHORIZED:
-                # message = error.response.me"API call-rate limit"
-                # args[0].logger.error(message)
-                raise error
             else:
                 raise error
-        return None
+
+    return wrapper
+
+
+def token_manager(func):
+    """Decorator to manage tokens for API access."""
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            # Check if access token exists
+            if not self.access_token:
+                # Generate a new token if it doesn't exist
+                self.get_token()
+                self.logger.info("Generated a new access token.")
+
+            # Call the original function with updated token
+            return func(self, *args, **kwargs)
+        except HTTPError as error:
+            if error.response.status_code == codes.UNAUTHORIZED:
+                self.logger.info("Unauthorized access token caught.")
+                self.get_token()
+                self.logger.info("Generated a new access token.")
+                return func(self, *args, **kwargs)
 
     return wrapper
 
@@ -61,9 +79,25 @@ class BrightspaceClient:
     logger = logging.getLogger("airbyte")
     version = "1.43"
 
+    def get_token(self):
+        payload = {
+            'client_auth_type': "CREDENTIALS_IN_BODY",
+            'token_url': self._refresh_endpoint,
+            'ingestor_source_update_url': self.ingestor_source_update_url,
+        }
+        try:
+            resp = self._make_request("POST", self.airflow_get_token, body=payload)
+        except Exception as err:
+            self.logger.error("Get token failed: %s", err)
+            raise err
+        auth = resp.json()
+        self.access_token = auth["access_token"]
+
     def __init__(
             self,
             instance_url: str,
+            airflow_get_token: str,
+            ingestor_source_update_url: str,
             refresh_token: str = None,
             token: str = None,
             client_id: str = None,
@@ -71,8 +105,11 @@ class BrightspaceClient:
             access_token: str = None,
             **kwargs: Any,
     ) -> None:
+        self._refresh_endpoint = "https://auth.brightspace.com/core/connect/token"
         self.refresh_token = refresh_token
         self.token = token
+        self.airflow_get_token = airflow_get_token
+        self.ingestor_source_update_url = ingestor_source_update_url
         self.client_id = client_id
         self.client_secret = client_secret
         self.access_token = access_token
@@ -98,33 +135,14 @@ class BrightspaceClient:
             if http_method == "GET":
                 resp = self.session.get(url, headers=headers, stream=stream, params=params)
             elif http_method == "POST":
-                resp = self.session.post(url, headers=headers, data=json.dumps(body))
+                resp = self.session.post(url, headers=headers, json=body)
             resp.raise_for_status()
         except HTTPError as err:
             self.logger.warning(f"http error body: {err.response.text}")
             raise
         return resp
 
-    def login(self):
-        login_url = f"https://auth.brightspace.com/core/connect/token"
-        login_body = {
-            "grant_type": "refresh_token",
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-            "refresh_token": self.refresh_token,
-        }
-        try:
-            resp = self._make_request("POST", login_url, body=login_body, headers={"Content-Type": "application/x-www-form-urlencoded"})
-        except HTTPError as err:
-            # if err.response.status_code == requests.codes.BAD_REQUEST:
-                # if error_message := AUTHENTICATION_ERROR_MESSAGE_MAPPING.get(err.response.json().get("error_description")):
-                # raise AirbyteTracedException(message=err.response.json().get("error_description"), failure_type=FailureType.config_error)
-            raise err
-        auth = resp.json()
-        self.access_token = auth["access_token"]
-        self.instance_url = auth["instance_url"]
-
-    @lru_cache(maxsize=None)
+    @token_manager
     @handle_http_errors
     def get_list_of_data_set(self) -> List[BSDataSet]:
         url = f"{self.url_base}/dataExport/list"
@@ -134,6 +152,7 @@ class BrightspaceClient:
         datasets = [BSDataSet(**dataset_data) for dataset_data in data]
         return datasets
 
+    @token_manager
     @handle_http_errors
     def create_export_job(self, payload: dict) -> BSExportJob:
         url = f"{self.url_base}/dataExport/create"
@@ -141,6 +160,7 @@ class BrightspaceClient:
         response = self._make_request(http_method="POST", url=url, headers=headers, body=payload)
         return BSExportJob(**response.json())
 
+    @token_manager
     @handle_http_errors
     def get_export_job_details(self, export_job_id: str) -> BSExportJob:
         url = f"{self.url_base}/dataExport/jobs/{export_job_id}"
@@ -148,6 +168,7 @@ class BrightspaceClient:
         response = self._make_request(http_method="GET", url=url, headers=headers)
         return BSExportJob(**response.json())
 
+    @token_manager
     @handle_http_errors
     def download_export_job(self, export_job_id: str):
         url = f"{self.url_base}/dataExport/download/{export_job_id}"
