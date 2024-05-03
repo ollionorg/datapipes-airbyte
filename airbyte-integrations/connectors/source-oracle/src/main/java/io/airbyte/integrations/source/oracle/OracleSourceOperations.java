fd91fcf41221ad72a36063e9c247cd76d96c38f0
Jatin Yadav <107487891+jatinyadav-cc@users.noreply.github.com>
2024-05-03 10:16:26 +0530

    fix: Oracle Datatype issue with Date, Time , Timestamp (#35)
    * fix: Oracle Datatype issue with Date, Time , Timestamp
    
    * files added

--- airbyte-integrations/connectors/source-oracle/src/main/java/io/airbyte/integrations/source/oracle/OracleSourceOperations.java
+++ airbyte-integrations/connectors/source-oracle/src/main/java/io/airbyte/integrations/source/oracle/OracleSourceOperations.java
@@ -4,10 +4,20 @@
 
 package io.airbyte.integrations.source.oracle;
 
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_COLUMN_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_COLUMN_TYPE;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_SCHEMA_NAME;
+import static io.airbyte.cdk.db.jdbc.JdbcConstants.INTERNAL_TABLE_NAME;
+
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.airbyte.cdk.db.jdbc.DateTimeConverter;
 import io.airbyte.cdk.db.jdbc.JdbcSourceOperations;
+import io.airbyte.protocol.models.JsonSchemaType;
+import java.sql.JDBCType;
 import java.sql.ResultSet;
+import java.util.Arrays;
+import oracle.jdbc.OracleType;
 import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +32,79 @@ public class OracleSourceOperations extends JdbcSourceOperations {
   @Override
   protected void putDate(ObjectNode node, String columnName, ResultSet resultSet, int index) throws SQLException {
     node.put(columnName, DateTimeConverter.convertToTimestamp(resultSet.getTimestamp(index)));
+  }
+
+  protected JDBCType safeGetJdbcType(final int columnTypeInt) {
+    try {
+      if (Arrays.asList(
+          OracleType.TIMESTAMP_WITH_LOCAL_TIME_ZONE.getVendorTypeNumber(),
+          OracleType.TIMESTAMP_WITH_TIME_ZONE.getVendorTypeNumber()
+      ).contains(columnTypeInt)) {
+        return JDBCType.TIMESTAMP_WITH_TIMEZONE;
+      }
+
+      if (Arrays.asList(
+          OracleType.BINARY_FLOAT.getVendorTypeNumber(),
+          OracleType.BINARY_DOUBLE.getVendorTypeNumber()
+      ).contains(columnTypeInt)) {
+        return JDBCType.DOUBLE;
+      }
+      return JDBCType.valueOf(columnTypeInt);
+    } catch (final Exception e) {
+      return JDBCType.VARCHAR;
+    }
+  }
+
+  @Override
+  public JDBCType getDatabaseFieldType(final JsonNode field) {
+    try {
+      if (Arrays.asList(
+          OracleType.TIMESTAMP_WITH_LOCAL_TIME_ZONE.getVendorTypeNumber(),
+          OracleType.TIMESTAMP_WITH_TIME_ZONE.getVendorTypeNumber()
+      ).contains(field.get(INTERNAL_COLUMN_TYPE).asInt())) {
+        return JDBCType.TIMESTAMP_WITH_TIMEZONE;
+      }
+
+      if (Arrays.asList(
+          OracleType.BINARY_FLOAT.getVendorTypeNumber(),
+          OracleType.BINARY_DOUBLE.getVendorTypeNumber()
+      ).contains(field.get(INTERNAL_COLUMN_TYPE).asInt())) {
+        return JDBCType.DOUBLE;
+      }
+
+      return JDBCType.valueOf(field.get(INTERNAL_COLUMN_TYPE).asInt());
+    } catch (final IllegalArgumentException ex) {
+      LOGGER.warn(String.format(
+          "Could not convert column: %s from table: %s.%s with type: %s. Casting to VARCHAR.",
+          field.get(INTERNAL_COLUMN_NAME),
+          field.get(INTERNAL_SCHEMA_NAME),
+          field.get(INTERNAL_TABLE_NAME),
+          field.get(INTERNAL_COLUMN_TYPE)));
+      return JDBCType.VARCHAR;
+    }
+  }
+
+  @Override
+  public JsonSchemaType getAirbyteType(final JDBCType jdbcType) {
+    return switch (jdbcType) {
+      case BIT, BOOLEAN -> JsonSchemaType.BOOLEAN;
+      case TINYINT, SMALLINT -> JsonSchemaType.INTEGER;
+      case INTEGER -> JsonSchemaType.INTEGER;
+      case BIGINT -> JsonSchemaType.INTEGER;
+      case FLOAT, DOUBLE -> JsonSchemaType.NUMBER;
+      case REAL -> JsonSchemaType.NUMBER;
+      case NUMERIC, DECIMAL -> JsonSchemaType.NUMBER;
+      case CHAR, NCHAR, NVARCHAR, VARCHAR, LONGVARCHAR -> JsonSchemaType.STRING;
+      case DATE -> JsonSchemaType.STRING_TIMESTAMP_WITHOUT_TIMEZONE;
+      case TIME -> JsonSchemaType.STRING_TIMESTAMP_WITH_TIMEZONE;
+      case TIMESTAMP_WITH_TIMEZONE -> JsonSchemaType.STRING_TIMESTAMP_WITH_TIMEZONE;
+      case TIMESTAMP -> JsonSchemaType.STRING_TIMESTAMP_WITHOUT_TIMEZONE;
+      case BLOB, BINARY, VARBINARY, LONGVARBINARY -> JsonSchemaType.STRING_BASE_64;
+      case ARRAY -> JsonSchemaType.ARRAY;
+      // since column types aren't necessarily meaningful to Airbyte, liberally convert all unrecgonised
+      // types to String
+      default -> JsonSchemaType.STRING;
+    };
   }
 
 }
